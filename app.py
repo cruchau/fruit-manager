f54a703eb8cfa6a024221f82c2e579cdd5f6af0c
cruchau <cruchau@amazon.com>
2025-08-13 14:22:01 +0100

    ajout de l'inventaire sous forme graphique (bar chart)

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import matplotlib.pyplot as plt
 from fruit_manager import *
 
 st.title("🍇 Dashboard de la plantation")
@@ -20,12 +21,26 @@ with st.sidebar:
     quantite_recolter = st.number_input("Quantité à récolter", min_value=1, step=1, key="quantite_recolter")
     
     if st.button("Récolter"):
-        recolter(inventaire, fruit_recolter, quantite_recolter)
-        ecrire_inventaire(inventaire)
+        inventaire = recolter(inventaire, fruit_recolter, quantite_recolter)
+        #ecrire_inventaire(inventaire)
         
 st.header("💰 Tresorerie")
 st.metric(label="Montant disponible", value=f"{tresorerie:.2f} $")
 
 st.header("📦 Inventaire")
+# Inventaire sous forme de tableau
 st.table(inventaire)
+# Inventaire sous forme de graphique
+fig, ax = plt.subplots()
+# Trier l'inventaire par quantite decroissante
+inventaire = dict(sorted(inventaire.item(), key=lambda item: item[1], reverse=True))
+ax.bar(inventaire.keys(), inventaire.values(), edgecolor='k')
+ax.set_xlabel("Fruits")
+ax.set_ylabel("Quantité")
+ax.set_title("Inventaire des Fruits")
+st.pyplot(fig)
+
+
+ecrire_inventaire(inventaire)
+ecrire_tresorerie(tresorerie)
 
